e0bb93ced59af249e6670280ba4ca22c67cee401
Marlon <mjohns12@mail.sfsu.edu>
2020-09-13 01:41:02 -0700

    fixed global font import

--- url-shortening-api/src/pages/_app.tsx
+++ url-shortening-api/src/pages/_app.tsx
@@ -1,5 +1,5 @@
-// import '../../styles/globals.css';
-import '../../sass/main.scss';
+import '../../styles/globals.css';
+// import '../../sass/main.scss';
 
 // import App from "next/app";
 import type { AppProps /*, AppContext */ } from 'next/app';
@@ -8,14 +8,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
       <Component {...pageProps} />
-      <style jsx global>
-        {`
-          @font-face {
-            font-family: 'Poppins';
-            src: url('../../public/fonts/Poppins-Regular.ttf');
-          }
-        `}
-      </style>
     </div>
   );
 }
--- url-shortening-api/src/pages/index.tsx
+++ url-shortening-api/src/pages/index.tsx
@@ -13,6 +13,10 @@ export default function Home() {
           sizes="32x32"
           href="../images/favicon-32x32.png"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Nav />
       <h1 className="super-color"> Hello I'm super color</h1>
