25200915b0e367ce431b22fdad2f4168c42a7cc5
Marlon Johnson <johnsonmarlon18@gmail.com>
2020-09-15 23:54:03 -0700

    header section completed

--- url-shortening-api/src/components/nav.tsx
+++ url-shortening-api/src/components/nav.tsx
@@ -1,47 +1,47 @@
 // @ts-ignore: corresponding type declarations.ts(2307)
-import logo from '../../images/logo.svg';
+import logo from '../../images/logo.svg'
 
 function Nav() {
-  return (
-    <>
-      <nav className="nav">
-        <ul className="nav__container">
-          <img className="nav__logo" src={logo} alt="logo" />
+	return (
+		<>
+			<nav className="nav">
+				<ul className="nav__container">
+					<img className="nav__logo" src={logo} alt="logo" />
 
-          <div className="nav__info">
-            <li className="nav__item nav__item--info">
-              <a href="#" className="nav__link">
-                Features
-              </a>
-            </li>
-            <li className="nav__item nav__item--info">
-              <a href="#" className="nav__link">
-                Pricing
-              </a>
-            </li>
-            <li className="nav__item nav__item--info">
-              <a href="#" className="nav__link">
-                Resources
-              </a>
-            </li>
-          </div>
+					<div className="nav__info">
+						<li className="nav__item nav__item--info">
+							<a href="#" className="nav__link">
+								Features
+							</a>
+						</li>
+						<li className="nav__item nav__item--info">
+							<a href="#" className="nav__link">
+								Pricing
+							</a>
+						</li>
+						<li className="nav__item nav__item--info">
+							<a href="#" className="nav__link">
+								Resources
+							</a>
+						</li>
+					</div>
 
-          <div className="nav__auth">
-            <li className="nav__item nav__item--auth">
-              <a href="#" className="nav__link">
-                Login
-              </a>
-            </li>
-          </div>
-          <li className="nav__item nav__item--active">
-            <a href="#" className="nav__link">
-              Sign Up
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </>
-  );
+					<div className="nav__auth">
+						<li className="nav__item nav__item--auth">
+							<a href="#" className="nav__link">
+								Login
+							</a>
+						</li>
+					</div>
+					<li className="nav__item nav__item--active">
+						<a href="#" className="nav__link">
+							Sign Up
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</>
+	)
 }
 
-export default Nav;
+export default Nav
--- url-shortening-api/src/pages/index.tsx
+++ url-shortening-api/src/pages/index.tsx
@@ -1,26 +1,27 @@
-import Head from 'next/head';
+import Head from 'next/head'
 // import '../../sass/pages/home.scss';
-import Nav from '../components/nav';
-import Layout from '../components/layout';
+import Nav from '../components/nav'
+import Header from '../components/header'
+import Layout from '../components/layout'
 
 export default function Home() {
-  return (
-    <Layout>
-      <Head>
-        <title>Frontend Mentor | Shortly URL shortening API Challenge</title>
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="../images/favicon-32x32.png"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Nav />
-      {/* <h1 className="super-color"> Hello I'm super color</h1> */}
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Head>
+				<title>Frontend Mentor | Shortly URL shortening API Challenge</title>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="../../images/favicon-32x32.png"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<Nav />
+			<Header />
+		</Layout>
+	)
 }
