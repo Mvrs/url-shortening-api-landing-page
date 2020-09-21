e8b15c466dd47ba0d194f40c9e1c608feb49f54e
Marlon Johnson <johnsonmarlon18@gmail.com>
2020-09-20 19:46:24 -0700

    completed boost section

---
+++ url-shortening-api/src/components/boost.tsx
@@ -0,0 +1,18 @@
+function Boost() {
+	return (
+		<>
+			<section className="boost">
+				<div className="boost__container">
+					<h1 className="boost__title">Boost your links today</h1>
+					<button>
+						<a href="#" className="u-button-active">
+							Get Started
+						</a>
+					</button>
+				</div>
+			</section>
+		</>
+	)
+}
+
+export default Boost
--- url-shortening-api/src/pages/index.tsx
+++ url-shortening-api/src/pages/index.tsx
@@ -1,29 +1,33 @@
-import Head from 'next/head'
+import Head from "next/head";
 // import '../../sass/pages/home.scss';
-import Nav from '../components/nav'
-import Header from '../components/header'
-import Layout from '../components/layout'
-import Branding from '../components/branding'
+import Nav from "../components/nav";
+import Header from "../components/header";
+import Layout from "../components/layout";
+import Branding from "../components/branding";
+import Boost from "../components/boost";
 
 export default function Home() {
-	return (
-		<Layout>
-			<Head>
-				<title>Frontend Mentor | Shortly URL shortening API Challenge</title>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="../../images/favicon-32x32.png"
-				/>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap"
-					rel="stylesheet"
-				/>
-			</Head>
-			<Nav />
-			<Header />
-			<Branding />
-		</Layout>
-	)
+    return (
+        <Layout>
+            <Head>
+                <title>
+                    Frontend Mentor | Shortly URL shortening API Challenge
+                </title>
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="../../images/favicon-32x32.png"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <Nav />
+            <Header />
+            <Branding />
+            <Boost />
+        </Layout>
+    );
 }
